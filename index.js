fe0d11c7735556dea7970ab9cd96c0797c9bce6f
EvaMH-jpg <55081847+EvaMH-jpg@users.noreply.github.com>
2021-01-20 16:23:02 +0100

    hg

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-window.addEventListener(
-  "scroll",
-  () => {
-    document.body.style.setProperty(
-      "--scroll",
-      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-    );
-  },
-  false
-);
+$(function() {
+    var rotation = 0, 
+        scrollLoc = $(document).scrollTop();
+    $(window).scroll(function() {
+        var newLoc = $(document).scrollTop();
+        var diff = scrollLoc - newLoc;
+        rotation += diff, scrollLoc = newLoc;
+        var rotationStr = "rotate(" + rotation + "deg)";
+        $(".gear").css({
+            "-webkit-transform": rotationStr,
+            "-moz-transform": rotationStr,
+            "transform": rotationStr
+        });
+    });
+})
+
+
+
+
+
+//window.addEventListener(
+//  "scroll",
+//  () => {
+//    document.body.style.setProperty(
+//      "--scroll",
+//      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+//    );
+//  },
+//  false
+//);
+//
 
 
 
