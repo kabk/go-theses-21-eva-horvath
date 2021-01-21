0b96bc1ba7a4f9af7788521d05041bad319f47a2
EvaMH-jpg <55081847+EvaMH-jpg@users.noreply.github.com>
2021-01-21 14:51:49 +0100

    walloffear

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   let navImage = document.getElementById('nav-image');
   new Propeller(navImage, {
@@ -8,48 +39,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-//$(function() {
-//    var rotation = 0, 
-//        scrollLoc = $(document).scrollTop();
-//    $(window).scroll(function() {
-//        var newLoc = $(document).scrollTop();
-//        var diff = scrollLoc - newLoc;
-//        rotation += diff, scrollLoc = newLoc;
-//        var rotationStr = "rotate(" + rotation + "deg)";
-//        $(".gear").css({
-//            "-webkit-transform": rotationStr,
-//            "-moz-transform": rotationStr,
-//            "transform": rotationStr
-//        });
-//    });
-//})
 
-
-
-
-
-// window.addEventListener(
-//   "scroll",
-//   () => {
-//     document.body.style.setProperty(
-//       "--scroll",
-//       window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-//     );
-//   },
-//   false
-// );
-
-
-
-
-
-
-//var div = document.querySelector("div");
-//var angle = 0;
-//document.onwheel = function(e) {
-//  if (e.deltaY) {                   // we have a wheel for vertical (common) direction
-//    e.preventDefault();
-//    angle += e.deltaY < 0 ? 4 : -4; // what direction?
-//    div.style.transform = "rotate(" + angle + "deg)"; // do something
-//  }
-//};
