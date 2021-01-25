b32f066a0bad5108f1869112dc585ec6e52a1df4
EvaMH-jpg <55081847+EvaMH-jpg@users.noreply.github.com>
2021-01-25 16:06:35 +0100

    all new

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},900);
+                $(this).animate({'opacity':'1'},800);
                     
             }
             
